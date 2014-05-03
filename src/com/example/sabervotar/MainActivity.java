2a1e9e53ea55772753b3e0737442ff7459efa243
dann1609 <danperzipa@hotmail.com>
2014-05-03 15:15:06 -0500

    commit
    Subida de la descripción del Proyecto

--- src/com/example/sabervotar/MainActivity.java
+++ src/com/example/sabervotar/MainActivity.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -22,6 +25,8 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+      
     }
 
 
